08f25db1670a2f78772e480ef47cf83f55672863
Noor Alam <nooralam192001@gmail.com>
2025-12-03 11:44:15 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
           <motion.img
             src="/bnplogo1.png"
             alt="BNP Logo"
-            className="h-14"
+            className="h-16 bg-white p-2 rounded shadow-lg"
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6 }}
